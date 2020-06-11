f8993c3881326cecc8c56310e44cf759c9fc1e6b
chenhui <947840093qq.com>
2020-06-11 10:47:08 +0800

    拦截器配置

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -33,6 +33,8 @@
     },
     methods: {
       login () {
+        var _this = this
+        console.log(this.$store.state)
         this.$axios
           .post('/login', {
             username: this.loginForm.username,
@@ -40,7 +42,10 @@
           })
           .then(successResponse => {
             if (successResponse.data.code === 200) {
-              this.$router.replace({path: '/index'})
+              // var data = this.loginForm
+              _this.$store.commit('login', _this.loginForm)
+              var path = this.$route.query.redirect
+              this.$router.replace({path: path === '/' || path === undefined ? '/index' : path})
             }
           })
           .catch(failResponse => {
