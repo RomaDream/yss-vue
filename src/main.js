355a9bf50dfa638e7abfd5ec3725b246d14425a9
chenhui <947840093qq.com>
2020-06-11 10:45:54 +0800

    前端拦截器配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 // 设置反向代理
 var axios = require('axios')
@@ -15,10 +16,27 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
+router.beforeEach((to, from, next)=>{
+  if(to.meta.requireAuth){
+    if(store.state.user.username){
+      next();
+    }else{
+      next({
+        path: 'login',
+        query: {redirect: to.fullpath}
+      })
+    }
+  }else{
+    next();
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
